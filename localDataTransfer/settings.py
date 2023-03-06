9bf313a80b9f36476b833a678f2abab6e30d4138
Moein Arabi <moein.mo81@gmail.com>
2023-03-06 12:16:57 +0330

    Update views and urls

--- localDataTransfer/settings.py
+++ localDataTransfer/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'transfer.apps.TransferConfig',
 ]
 
 MIDDLEWARE = [
--- transfer/views.py
+++ transfer/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello')
+    context = {
+        'title': 'Transfer'
+    }
+    return render(request, 'transfer/index.html', context)
+
+
+def upload(request):
+    context = {
+        'title': 'Upload | Transfer'
+    }
+    return render(request, 'transfer/upload.html', context)
