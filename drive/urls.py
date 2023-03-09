bc37c503bf7104f7085ce824f496a19a486a0cd7
Moein Arabi <moein.mo81@gmail.com>
2023-03-09 23:19:51 +0330

    Add API endpoints

--- drive/urls.py
+++ drive/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('api/directory/', views.api_get_directory, name='directory'),
+    path('api/file/', views.api_file, name='file'),
+    path('api/doc', views.api_doc, name='doc'),
 ]
--- drive/views.py
+++ drive/views.py
@@ -1,5 +1,9 @@
+import re
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+
+import os
 
 
 # Create your views here.
@@ -8,3 +12,38 @@ def index(request):
         'title': 'Drive'
     }
     return render(request, 'drive/index.html', context)
+
+
+def api_doc(request):
+    context = {
+        'title': 'API DOC | Drive'
+    }
+    return render(request, 'drive/api_doc.html', context)
+
+
+def api_get_directory(request):
+    path = request.GET['path']
+    with os.scandir(path) as it:
+        folder_list = list(it)
+        response = {
+            'count': len(folder_list),
+            'directory': [],
+        }
+        for f in folder_list:
+            obj = {
+                'name': f.name,
+                'is_directory': f.is_dir(),
+                'is_file': f.is_file(),
+                'path': f'{path}/{f.name}',
+                'size': f.stat().st_size,
+                'extension': None,
+            }
+            if obj['is_file']:
+                result = re.findall(r'\.[\w\d]*$', f.name)
+                obj['extension'] = result[0] if result else None
+            response['directory'].append(obj)
+        return JsonResponse(response)
+
+
+def api_file(request):
+    pass
