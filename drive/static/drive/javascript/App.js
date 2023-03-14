3afee8b486285724a6653001b3243d7232ea8569
Moein Arabi <moein.mo81@gmail.com>
2023-03-14 10:02:31 +0330

    Go forwards in directory

--- drive/static/drive/javascript/App.js
+++ drive/static/drive/javascript/App.js
@@ -7,11 +7,12 @@ import {Directory} from "./Directory";
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {'directory': null}
+        this.state = {'directory': null, 'path': 'D:/'}
+        this.changePath = this.changePath.bind(this);
     }
 
-    async getDirectories() {
-        const url = 'http://localhost:8000/drive/api/directory?path=D:/';
+    async getDirectories(path) {
+        const url = `http://localhost:8000/drive/api/directory?path=${path}`;
         const init = {
             headers: new Headers({
                 'Accept': 'application/json',
@@ -25,8 +26,14 @@ class App extends React.Component {
         throw new Error('The response code is not ok');
     }
 
+    changePath(newPath) {
+        this.getDirectories(newPath)
+            .then(resolve => this.setState({'directory': resolve.directory, 'path': newPath}))
+            .catch(reject => console.log(reject));
+    }
+
     componentDidMount() {
-        this.getDirectories()
+        this.getDirectories(this.state.path)
             .then(resolve => this.setState({'directory': resolve.directory}))
             .catch(reject => console.log(reject));
     }
@@ -38,7 +45,8 @@ class App extends React.Component {
 
         let directoriesComponent = []
         for (const directory of this.state.directory) {
-            directoriesComponent.push(<Directory isFile={directory.is_file} name={directory.name} />);
+            directoriesComponent.push(<Directory isFile={directory.is_file} name={directory.name}
+                                                 path={directory.path} changePath={this.changePath} />);
         }
         return (
             <div className='directory-grid'>
--- drive/static/drive/javascript/Directory.js
+++ drive/static/drive/javascript/Directory.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 export class Directory extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onClickHandler = this.onClickHandler.bind(this);
+    }
+
+    onClickHandler(_) {
+        this.props.changePath(this.props.path);
+    }
+
     render() {
         let imageSrc;
         if (this.props.isFile === false) {
@@ -8,7 +17,7 @@ export class Directory extends React.Component {
         }
 
         return (
-            <div className="directory-card">
+            <div className="directory-card" onClick={this.onClickHandler}>
                 <img src={imageSrc} className="directory-img" alt="Directory icon" />
                 <div className="directory-name">{this.props.name}</div>
             </div>
