55aceea7d95c025baa8fcd88df8792fb87299bd2
Moein Arabi <moein.mo81@gmail.com>
2023-07-09 12:29:56 +0330

    Update webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
-const path = require('path');
+let mode = 'development';
 
-module.exports = {
-  mode: 'development',
-  entry: './drive/static/drive/compiled/App.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, './drive/static/drive/dist'),
-  },
-};
+module.exports = [
+    {
+        mode: mode,
+        entry: './drive/static/drive/compiled/App.js',
+        output: {
+            filename: 'drive.js',
+            path: __dirname + '/drive/static/drive/dist',
+        }
+    },
+    {
+        mode: mode,
+        entry: './accounts/static/accounts/compiled/Login.js',
+        output: {
+            filename: 'login.js',
+            path: __dirname + '/accounts/static/accounts/dist',
+        }
+    },
+];
+module.exports.parallelism = 2;
