77d935e1a5b32b7276d2d95bc657c1d6c23691de
Moein Arabi <moein.mo81@gmail.com>
2023-07-11 11:27:04 +0330

    Add django admin

--- babyCloud/settings.py
+++ babyCloud/settings.py
@@ -29,19 +29,18 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'drive',
-    'corsheaders',
     'main',
     'accounts',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,10 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:8000',
 ]
 
 ROOT_URLCONF = 'babyCloud.urls'
