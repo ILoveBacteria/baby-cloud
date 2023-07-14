0d6a788d68dd77d55647e8a9d776236d499e4323
Moein Arabi <moein.mo81@gmail.com>
2023-07-14 11:50:44 +0330

    Add name to urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('login/', login_view),
-    path('logout/', logout_view),
-    path('signup/', signup_view)
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('signup/', signup_view, name='signup')
 ]
